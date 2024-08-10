f99436eab99aba45fe827b149c156ded99bbf875
AdrianMares <adrianmares.dev@gmail.com>
2024-08-09 23:09:53 -0400

    added 8kyu challenges

--- 8Kyu/8kyupractice.js
+++ 8Kyu/8kyupractice.js
@@ -10,7 +10,7 @@ function evenOrOdd(number) {
     }
   }
 
-  
+
 //#2 Description
 /*Complete the function which returns the weekday according to the input number:
 1 returns "Sunday"
@@ -40,5 +40,53 @@ function whatday(num) {
       return "Saturday"
     }else {
       return "Wrong, please enter a number between 1 and 7"
+    }
+  }
+
+
+//#3 Description
+//Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+//My Solution
+  function greet(name){
+    return `Hello, ${name} how are you doing today?`
+  }
+
+
+//#4 Description
+//Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+
+//My Solution
+  function problem(x){
+    if(typeof x === 'string') {
+      return "Error"
+    }else{
+      return x * 50 + 6
+    }
+  }
+
+
+//#5 Description
+/*Write function bmi that calculates body mass index (bmi = weight / height2).
+
+if bmi <= 18.5 return "Underweight"
+
+if bmi <= 25.0 return "Normal"
+
+if bmi <= 30.0 return "Overweight"
+
+if bmi > 30 return "Obese"*/
+
+//My Solution
+function bmi(weight, height) {
+    let bmi = weight/(height ** 2);
+    if(bmi <= 18.5) {
+      return "Underweight"
+    }else if(bmi <= 25.0) {
+      return "Normal"
+    }else if(bmi <= 30.0) {
+      return "Overweight"
+    }else {
+      return "Obese"
     }
   }
