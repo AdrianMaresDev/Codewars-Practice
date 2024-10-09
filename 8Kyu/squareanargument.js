c7f1c13f3ec7c2fc5c2ba6a9d8c06a431690e966
AdrianMares <adrianmares.dev@gmail.com>
2024-10-09 08:02:39 -0400

    added new challenges

---
+++ 8Kyu/squareanargument.js
@@ -0,0 +1,9 @@
+//Description
+//You have to write a function that takes an argument and returns the square of it.
+
+//My Solution
+function square (num) {
+    return num ** 2;
+  }
+  
+  console.log(square(4));
